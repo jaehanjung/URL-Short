2a8df2ec7a394798847aa8220810842801a52b47
jaehanjung <wjdwogks23@gmail.com>
2018-05-16 15:53:11 +0900

    예제 데이터 테스트

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const randomstring = require('randomstring')
 
 const app = express()
+
+const urls = [
+  {
+    slug: randomstring.generate(8),
+    longUrl: 'https://www.naver.com'
+  }
+]
 
 app.use('/static', express.static('public'))
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {urls});
+})
+app.get('/:slug', (req, res)=>{
+ const urlItem = urls.find(item => item.slug === req.params.slug) 
+ if (urlItem){
+   res.redirect(301, urlItem.longUrl)
+ } else {
+   res.status(404)
+   res.send('404 not found')
+ }
 })
 
-app.listen(3001, () => {
+app.listen(3002, () => {
   console.log('listening...')
 })
