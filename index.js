3b38c401c2906f561157b6f8545b4aa9865ef06c
jaehanjung <wjdwogks23@gmail.com>
2018-05-16 16:31:08 +0900

    폼 작성

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const randomstring = require('randomstring')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -13,11 +14,26 @@ const urls = [
 
 app.use('/static', express.static('public'))
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
  const host = req.get('host')
   res.render('index.ejs', {host, urls});
 })
+
+app.get('/new', (req, res)=>{
+  res.render('new.ejs')
+})
+
+app.post('/new', (req, res)=>{
+ const urlItem = {
+  longUrl: req.body.longUrl,
+  slug: randomstring.generate(8)
+ } 
+ urls.push(urlItem)
+ res.redirect('/')
+})
+
 app.get('/:slug', (req, res)=>{
  const urlItem = urls.find(item => item.slug === req.params.slug) 
  if (urlItem){
@@ -27,6 +43,8 @@ app.get('/:slug', (req, res)=>{
    res.send('404 not found')
  }
 })
+
+
 
 app.listen(3002, () => {
   console.log('listening...')
