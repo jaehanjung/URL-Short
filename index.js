5c886057a86a339fe7be52f893ae2a5828648f16
jaehanjung <wjdwogks23@gmail.com>
2018-05-16 15:16:53 +0900

    morgan 적용

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 
 
 const app = express()
 
 app.use('/static', express.static('public'))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('listening...')
 })
